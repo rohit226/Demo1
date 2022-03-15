785ca1ded57167790206404b04079ef06bbc4848
rohit226 <57681273+rohit226@users.noreply.github.com>
2022-03-15 22:42:28 +0530

    c++ file

---
+++ demo1.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+
+// int main()
+// {
+//     bool status 25;
+//     printf("\nValue of Status = %d", status);
+//     printf("\nSize of Status = %d", sizeof(status));
+
+//     return 0;
+// }
+
+
+#include <iostream>
+
+int main()
+{
+    /*bool status = false;
+    printf("\nValue of Status = %d", status);
+    printf("\nSize of Status = %d", sizeof(status));
+*/
+
+    wchar_t name = 'a';
+    printf("\nValue of Status = %c", name);
+    printf("\nSize of Status = %d", sizeof(name));
+    return 0;
+}
+
+            
+
+            
