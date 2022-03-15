abdecf7247734feafa83e05adc0fee06b6f6bcb6
rohit226 <57681273+rohit226@users.noreply.github.com>
2022-03-15 12:14:32 +0530

    c program

---
+++ control _statement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    double amount;
+    printf("enter the amount");
+    scanf("%lf",&amount);
+
+    if(discount% = (discount/amount)*100)
+    {
+        printf("final bill with discount");
+    }
+
+    else
+    {
+        printf("no discounted bill");
+    }
+    return 0;
+}
